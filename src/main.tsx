29178694658a8066daf0824498eb9a8c2ed7235f
unknown <you@example.com>
2025-12-14 13:33:29 +0100

    Fix router and base path

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
